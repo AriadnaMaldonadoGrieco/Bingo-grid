ab4959832b1da75d7eed0078146a1bd3a583bda5
Ariadna Maldonado <ariadnamaldonadogrieco@gmail.com>
2024-02-22 11:04:42 -0300

    setting up project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+
+
+function App() {
+  return(
+    <>
+    <button className="btn btn-warning p-3 m-5">hi!</button>
+    </>
+  )
+}
+
+export default App
