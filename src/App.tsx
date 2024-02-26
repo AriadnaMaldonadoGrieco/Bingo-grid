98aaa714f2503775d2ba65005b3513e01c8c25f1
Ariadna Maldonado <ariadnamaldonadogrieco@gmail.com>
2024-02-26 11:30:57 -0300

    mejora de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
-import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
-
-
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import HxH from "./images/hh.jpg";
 
 function App() {
-  return(
+  return (
     <>
-    <button className="btn btn-warning p-3 m-5">hi!</button>
+      <div className="imageBanner">
+        <img src={HxH} className="img-fluid d-flex align-self-center m-auto" alt="hunter hunter protagonists" />
+      </div>
+
+      <div className="bingoContainer text-center d-flex align-items-center grid gap-1">
+        <div className="col bingoRows" >
+          <div className="row bingoSquares">one</div>
+          <div className="row bingoSquares">two</div>
+          <div className="row bingoSquares">three</div>
+          <div className="row bingoSquares">four</div>
+          <div className="row bingoSquares">five</div>
+        </div>
+        <div className="col bingoRows" >
+          <div className="row bingoSquares">one</div>
+          <div className="row bingoSquares">two</div>
+          <div className="row bingoSquares">three</div>
+          <div className="row bingoSquares">four</div>
+          <div className="row bingoSquares">five</div>
+        </div>
+        <div className="col bingoRows" >
+          <div className="row bingoSquares">one</div>
+          <div className="row bingoSquares">two</div>
+          <div className="row bingoSquares">
+            <div className="star"></div>
+          </div>
+          <div className="row bingoSquares">four</div>
+          <div className="row bingoSquares">five</div>
+        </div>
+        <div className="col bingoRows" >
+          <div className="row bingoSquares">one</div>
+          <div className="row bingoSquares">two</div>
+          <div className="row bingoSquares">three</div>
+          <div className="row bingoSquares">four</div>
+          <div className="row bingoSquares">five</div>
+        </div>
+        <div className="col bingoRows" >
+          <div className="row bingoSquares">one</div>
+          <div className="row bingoSquares">two</div>
+          <div className="row bingoSquares">three</div>
+          <div className="row bingoSquares">four</div>
+          <div className="row bingoSquares">five</div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
